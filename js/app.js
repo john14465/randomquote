652c72afbda6e15902117083525c62f4661b7399
Adam Johnson <john14465@hotmail.com>
2017-10-26 21:21:44 -0500

    finished quote machine

--- js/app.js
+++ js/app.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
-    $.getJSON('https://talaikis.com/api/quotes/', function(q){
-        
+    $.getJSON("https://talaikis.com/api/quotes", function(res){
+      var num = 0;
+      
+      $('#quote').html('"'+res[num].quote+'"');
+      $('#author').html('- ' + res[num].author + '');
+      
+      function randomNum(){
+        num = Math.floor((Math.random() * 100) + 1);
+        $('#quote').html('"'+res[num].quote+'"');
+        $('#author').html('- ' + res[num].author + '');
+        $('#tweet').href = "https://twitter.com/intent/tweet/?text=" + res[num].quote;
+      };
+      
+      function tweet() {
+        $("#tweet").attr("href", "https://twitter.com/intent/tweet/?text=" + res[num].quote + " - " + res[num].author);
+      }
+      $('#new').click(randomNum);
+      $('#tweet').click(tweet);
     });
-});
+  });
